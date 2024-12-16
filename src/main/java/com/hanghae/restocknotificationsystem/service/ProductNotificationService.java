226c64fa1cfbf59639d7f4ffe9a8def3731b9794
mjyoo <ryuminji0353@gmail.com>
2024-12-16 06:25:32 -0800

    [refactor] 메서드 분리 및 중복 코드 제거

--- src/main/java/com/hanghae/restocknotificationsystem/service/ProductNotificationService.java
+++ src/main/java/com/hanghae/restocknotificationsystem/service/ProductNotificationService.java
@@ -26,11 +26,10 @@ public class ProductNotificationService {
 
     //재입고 알림을 설정한 유저들에게 재입고 알림 전송
     public RestockNotificationResponse sendRestockNotification(Long productId) {
+        //상품 조회 및 검증
+        Product product = getProductOrThrowException(productId);
 
-        Product product = productRepo.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
-
-        //재입고 알림을 전송하기 전, 상품의 재입고 회차를 1 증가시킴
+        //재입고 회차 증가
         product.setRestockCount(product.getRestockCount() + 1);
         productRepo.save(product);
 
@@ -39,34 +38,47 @@ public class ProductNotificationService {
         notificationHistoryRepo.save(history);
 
         //상품에 대해 알림을 설정한 유저 목록 조회
-        List<ProductUserNotification> userNotificationList = userNotificationRepo.findByProductIdAndActiveTrue(productId);
+        List<ProductUserNotification> notifiedUsers = userNotificationRepo.findByProductIdAndActiveTrue(productId);
 
         //상품이 재입고 되었을 때, 유저에게 알림 전송 시도
-        for (ProductUserNotification userNotification : userNotificationList) {
-            if (!product.isStock()) { //재고가 없는 경우
-                System.out.println("재고가 부족하여 알림 전송을 중단합니다.");
-                history.setStatus(NotificationStatus.CANCELED_BY_SOLD_OUT);
-                notificationHistoryRepo.save(history);
-                break;
-            }else{ //재고가 있는 경우
-                history.setStatus(NotificationStatus.COMPLETED);
-                sendNotificationToUser(userNotification, product, product.getRestockCount());
-                notificationHistoryRepo.save(history);
+        for (ProductUserNotification user : notifiedUsers) {
+            try{
+                if (!product.isStock()) { //재고가 없는 경우
+                    updateNotificationStatus(history,NotificationStatus.CANCELED_BY_SOLD_OUT);
+                    break;
+                }
+                //알림 발송
+                sendNotificationToUser(user, product, product.getRestockCount());
+                //마지막으로 알림을 보낸 유저 기록
+                history.setLastSentUserId(user.getUserId());
+                updateNotificationStatus(history, NotificationStatus.COMPLETED);
+            }catch (Exception e){
+                updateNotificationStatus(history, NotificationStatus.CANCELED_BY_ERROR);
+                throw e;
             }
         }
         return new RestockNotificationResponse(history);
     }
 
+    private void updateNotificationStatus(ProductNotificationHistory history, NotificationStatus status) {
+        history.setStatus(status);
+        notificationHistoryRepo.save(history);
+    }
+
+    private Product getProductOrThrowException(Long productId) {
+        Product product = productRepo.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
+        return product;
+    }
+
     //알림 전송 (실제로는 메시지를 보내지 않음, 상태만 기록)
-    public void sendNotificationToUser(ProductUserNotification userNotification, Product product, int restockCount) {
+    private void sendNotificationToUser(ProductUserNotification userNotification, Product product, int restockCount) {
         // 재입고 알림 전송의 상태 테스트를 위한 재고 감소 코드
         if (product.getQuantity() > 0) {
             product.setQuantity(product.getQuantity() - 1);
             productRepo.save(product);
-            System.out.println("재고 감소 완료. 현재 재고: " + product.getQuantity());
-        } else {
-            System.out.println("재고 부족. 알림 전송 중단.");
         }
+        System.out.println("재고가 부족하여 알림 전송을 중단합니다.");
 
         //알림 전송 후 기록 저장
         ProductUserNotificationHistory notificationHistory = new ProductUserNotificationHistory(product.getId(), userNotification.getUserId(), restockCount);
